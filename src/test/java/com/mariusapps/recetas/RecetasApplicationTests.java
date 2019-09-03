451c49bba61098ff918f88de8c3be848c9c1c7b3
whiteolga48 <whiteolga48@gmail.com>
2019-09-03 13:14:23 +0200

    commit iniciaL

---
+++ src/test/java/com/mariusapps/recetas/RecetasApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mariusapps.recetas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecetasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
