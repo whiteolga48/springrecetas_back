b6778e0ad49467d1cbf593d3e5005d806adaabed
rauxasoft <jordia4@gmail.com>
2019-09-05 09:30:05 +0200

    arreglos

--- src/main/java/com/mariusapps/recetas/backend/integration/repositories/IngredienteRepository.java
+++ src/main/java/com/mariusapps/recetas/backend/integration/repositories/IngredienteRepository.java
@@ -1,9 +1,11 @@
 package com.mariusapps.recetas.backend.integration.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.mariusapps.recetas.backend.integration.model.Ingrediente;
 
+@Repository
 public interface IngredienteRepository extends JpaRepository<Ingrediente, Long>{
 
 }
