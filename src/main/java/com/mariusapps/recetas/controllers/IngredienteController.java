2f5d179e98124d54e812f0ec4d056ab747e29dc5
rauxasoft <jordia4@gmail.com>
2019-09-27 12:37:18 +0200

    endpoint para crear/eliminar ingredientes

--- src/main/java/com/mariusapps/recetas/controllers/IngredienteController.java
+++ src/main/java/com/mariusapps/recetas/controllers/IngredienteController.java
@@ -4,8 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mariusapps.recetas.backend.integration.model.Ingrediente;
@@ -30,5 +33,28 @@ public class IngredienteController {
 		return optional.isPresent() ? optional.get() : null;
 		
 	}
+	
+	@RequestMapping(value="/ingredientes",
+			        method=RequestMethod.POST,
+			        consumes=MediaType.APPLICATION_JSON_VALUE,
+			        produces=MediaType.APPLICATION_JSON_VALUE)
+	public Ingrediente create(@RequestBody Ingrediente ingrediente) {
+		
+		Ingrediente createdIngrediente = ingredienteRepository.save(ingrediente);
+		
+		return createdIngrediente;
+	}
+	
+	
+	@RequestMapping(value="/ingredientes/{id}",
+					method=RequestMethod.DELETE)
+	public String delete(@PathVariable int codigo) {
+		
+		ingredienteRepository.deleteById((long)codigo); 
+		
+		return "OK";
+	}
+	
+	
 	
 }
